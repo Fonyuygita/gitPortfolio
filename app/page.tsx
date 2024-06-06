cd81f08d18b696ac41379555f438d7070e683370
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-06-06 08:59:40 -0700

    wworjing with Grids cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/ui/Grid";
+import { navItems } from "@/data";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden  mx-auto sm:px-10">
       <div className="text-white text-3xl max-w-7xl w-full">
+        <FloatingNav
+          className=""
+          navItems={navItems}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Spotlight } from './ui/Spotlight'
+import { Spotlight } from './ui/SpotLight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import Image from 'next/image'
+// import ShimerButton from './ui/ShimerButton'
+import MagicButton from './ui/ShimerButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { SparklesCore } from './ui/Sparkles'
 // primary section you see when first visit the page
 const Hero = () => {
     return (
         <div className='pb-20 pt-36'>
+            <div className="h-[2rem] -z-20  w-screen bg-transparent flex flex-col items-center justify-center overflow-hidden rounded-md">
+                <div className="w-full absolute inset-0 h-screen top-0 left-0  ">
+                    <SparklesCore
+                        id="tsparticlesfullpage"
+                        background="transparent"
+                        minSize={0.6}
+                        maxSize={1.4}
+                        particleDensity={100}
+                        className="w-full h-full"
+                        particleColor="#FFFFFF"
+                    />
+                </div>
+
+            </div>
             <div>
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
@@ -24,6 +42,13 @@ const Hero = () => {
                         words='Following the Foot steps of FeynMan, But in Cs'
                     />
                     <p className="text-center md:tracking-wider mt-2 mb-4 text-sm md:text-lg lg:text-2xl">{" Hi I'm Gita, A developer"}</p>
+                    <a href="#about">
+                        <MagicButton
+                            title="About Gita"
+                            icon={<FaLocationArrow />}
+                            position='right'
+                        />
+                    </a>
                 </div>
             </div>
 
