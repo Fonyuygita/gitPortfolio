b2234ee59d4b08b6123bd45d174aaa992b28444d
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-06-30 05:34:39 -0700

    added sentry

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,10 +2,11 @@ import { projects } from '@/data'
 import React from 'react'
 import { div } from 'three/examples/jsm/nodes/Nodes.js'
 import { PinContainer } from './ui/3d-pin'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
     return (
-        <div className='py-20'>
+        <div className='py-20' id="projects">
             <h1 className="heading">
                 Ma Bolo {' '}
                 <span className="text-purple">Them</span>
@@ -13,12 +14,12 @@ const RecentProjects = () => {
 
             {/* div to show our project card */}
 
-            <div className="flex flex-wrap items-center justify-center p-4 gap-16 text-lg">
+            <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 text-lg">
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
 
-                    <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center sm:w-96 w-[80vw]  justify-center">
-                        <PinContainer title={title} href={link}>
-                            <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10]'>
+                    <div key={id} className="lg:min-h-[32.5rem] sm:h-[41rem] h-[32rem] sm:w-[570px] flex items-center  w-[80vw]  justify-center">
+                        <PinContainer title={link} href={link}>
+                            <div className='relative flex items-center sm:w-[570px] justify-center w-[80vw] sm:h-[40vh] overflow-hidden h-[30vh] lg:h-[30vh] mb-10]'>
                                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                     <img src="/bg.png" alt="bg-image" />
 
@@ -33,10 +34,17 @@ const RecentProjects = () => {
                             <div className="flex items-center justify-between mt-7 mb-3">
                                 <div className="flex items-center">
                                     {iconLists.map((icon) => (
-                                        <div key={icon} className="border border-white/[10.2] rounded-full">
-                                            <img src={icon} alt={icon} classsName="p-2" />
+                                        <div key={icon} className="border border-white/[10.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center" style={{
+                                            transform: `translateX({ -5 * index * 2}px)`
+                                        }}>
+                                            <img src={icon} alt={icon} className="p-2" />
                                         </div>
                                     ))}
+                                </div>
+
+                                <div className="flex justify-center items-center">
+                                    <p className="flex lg:text-xl text-sm">Go live</p>
+                                    <FaLocationArrow className="ms-3" color="#CBACF9" />
                                 </div>
                             </div>
 
